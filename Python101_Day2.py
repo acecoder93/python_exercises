5b838ee6346a8500913b7d2c67ceb3f23a06944e
Anuj <anujsaheba@gmail.com>
2018-11-13 19:26:46 -0600

    Python Classroom Exercises

---
+++ Python101_Day2.py
@@ -0,0 +1,134 @@
+
+# 1 - Hello You!
+
+# name = input('What is your name?')
+# print("Hello " + name)
+
+# 2 - HELLO, YOU!
+# name = (input('What is your name?'))
+# print (name.upper())
+# print ("YOUR NAME HAS " + str(len(name)) + " LETTERS IN IT!")
+
+# 3 - Madlib
+# name = input('What is your name?')
+# animal = input('What is your favorite animal?')
+
+# madlib = "%s\'s favorite animal is a one-legged %s" % (name, animal)
+# print (madlib)
+
+# 4 - Day of the Week
+# please = int(input('Please submit a number from 0 to 6'))
+
+# if please == 0:
+#     print ("Sunday")
+# elif please == 1:
+#     print ("Monday")
+# elif please == 2:
+#     print ("Tuesday")
+# elif please == 3:
+#     print ("Wednesday")
+# elif please == 4:
+#     print ("Thursday")
+# elif please == 5:
+#     print ("Friday")
+# elif please == 6:
+#     print ("Saturday")
+
+# 5- Work or Sleep In?
+# please = int(input('Please submit a number from 0 to 6'))
+
+# day = ''
+# if please == 0:
+#     day = "Sunday"
+# elif please == 1:
+#     day = "Monday"
+# elif please == 2:
+#     day = "Tuesday"
+# elif please == 3:
+#     day = "Wednesday"
+# elif please == 4:
+#     day = "Thursday"
+# elif please == 5:
+#     day = "Friday"
+# elif please == 6:
+#     day = "Saturday"
+
+# if day == "Sunday":
+#     print ("Sleep in")
+# elif day == "Monday":
+#     print ("Go to work")
+# elif day == "Tuesday":
+#     print ("Go to work")
+# elif day == "Wednesday":
+#     print ("Go to work")
+# elif day == "Thursday":
+#     print ("Go to work")
+# elif day == "Friday":
+#     print ("Go to work")
+# elif day == "Saturday":
+#     print ("Sleep in")
+
+# 6 - Celsius to Fahrenheit
+# degrees = float(input('What is the temperature in Celsius?'))
+# Fahrenheit = degrees * 1.8 + 32
+# print (Fahrenheit)
+
+# 7 - Tip Calculator
+
+# total_bill = input('What is the total bill amount?')
+# level_service = str.lower(input('Please rate the level of service from the following: good, fair, or bad'))
+
+
+# if level_service == "good":
+#     tip_amount = (float(total_bill) * .20)
+# elif level_service == "fair":
+#     tip_amount = (float(total_bill) * .15)
+# elif level_service == "bad":
+#     tip_amount = (float(total_bill) * .10)
+
+# total_bill_cost = float(total_bill) + tip_amount
+# print (total_bill_cost)
+
+# 8 - Tip Calculator 2
+
+# total_bill = input('What is the total bill amount?')
+# people = int(input('Split this check amongst how many people?'))
+# level_service = str.lower(input('Please rate the level of service from the following: good, fair, or bad'))
+
+
+# if level_service == "good":
+#     tip_amount = (float(total_bill) * .20)
+# elif level_service == "fair":
+#     tip_amount = (float(total_bill) * .15)
+# elif level_service == "bad":
+#     tip_amount = (float(total_bill) * .10)
+
+# total_bill_cost = float(total_bill) + tip_amount
+# print (float(total_bill_cost) / people)
+
+# 9 - 1 to 10
+
+# count = 0
+# while count < 10:
+#     count += 1
+#     print (count)
+
+# # 10 - How many coins?
+
+# coins = 0
+# print ("You have %d coins" %(coins))
+
+# answer = str.lower(raw_input('Do you want another?'))
+
+# while answer == "yes":
+#     coins += 1
+#     print ('You have %d coins' % (coins))
+#     answer = str.lower(raw_input('Do you want a coin?'))
+
+# if answer == "no":
+#     print ("Bye")
+# else:
+#     print("invalid")
+
+
+    
