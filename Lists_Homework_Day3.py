837ea84420ac396399614f903f815ed8fd17fb6d
Anuj <anujsaheba@gmail.com>
2018-11-14 22:54:57 -0600

    Lists Homework Day 3

---
+++ Lists_Homework_Day3.py
@@ -0,0 +1,88 @@
+#  1. Sum the Numbers
+
+# myList = [2, 23, 123, 28]
+# print (sum(myList))
+
+# 2. Largest Number
+
+# myList = [1, 4, 6, 12, 5123, -1, -3]
+# print (max(myList))
+
+# 3. Smallest Number
+
+# myList = [-23, -20, -4, 0, 123, 21]
+# print (min(myList))
+
+# 4. Even Numbers
+# myList = [3, 5, 12, 18, 4, 7, 6]
+# for number in myList:
+#     if number % 2 == 0:
+#      print (number)
+    
+# 5. Positive Numbers
+# myList = [-10, -5, -2, 0, 1, 5, 10]
+# for number in myList:
+#     if number > 0:
+#         print (number)
+#     else:
+       
+
+# 6. Positive Numbers II
+# myList = [-10, -5, -1, 0, 1, 5, 10]
+# empty = []
+
+# for number in myList:
+#     if number > 0:
+#         empty.append(number)
+#     else:
+#         ""
+# print (empty)
+
+# 7. Multiply a List
+
+# myList = [3, 6, 9, 12]
+# var_factor = 5
+# newList = []
+
+# for number in myList:
+#     total = number * var_factor
+#     newList.append(total)
+# print (newList)
+
+# 8. Multiply Vectors
+# list1 = [2, 4, 5]
+# list2 = [2, 3, 6]
+# list3 = []
+
+# for num1 in list1:
+#     for num2 in list2:
+#         total = num1*num2
+#     list3.append(total)
+# print(list3)
+
+# 9. Matrix Addition (NEED TO WORK ON)
+# matrix1 = [ [2, -2], [5, 3]]
+# matrix2 = [ [1, 2], [3, 4]]
+# matrix3 = [ [0, 0], [0, 0]]
+
+# for num1 in range(len(matrix1)):
+#     for num2 in range(len(matrix2)):
+#         matrix3[num1][num2] = matrix1[num1][num2] + matrix2[num1][num2]
+# print (matrix3)
+
+
+# 10. Matrix Addition II - Need to revisit
+# matrix1 = [ [2, -2], [5, 3]]
+# matrix2 = [ [1, 2], [3, 4]]
+# matrix3 = [ [0, 0], [0, 0]]
+
+# for num1 in range(len(matrix1)):
+#     for num2 in range(len(matrix2)):
+#         matrix3[num1][num2] = matrix1[num1][num2] + matrix2[num1][num2]
+# print (matrix3)
+
+# 11. De-dup - Need to revisit and determine how to solve with our commands
+# myList = [1, 4, 5, 3, 10, 8, 4, 1, 4, 3, 2]
+# newList = set(myList)
+# print(newList)
+
