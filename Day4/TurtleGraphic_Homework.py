4036f089a17f0187f48e0e5be3efa2200862f409
Anuj <anujsaheba@gmail.com>
2018-11-15 19:35:37 -0600

    First Draft

---
+++ Day4/TurtleGraphic_Homework.py
@@ -0,0 +1,166 @@
+#Turtle Exercises
+
+# Exercise 0 - Square Centered on the Screen
+# from turtle import *
+# # move into position
+# up()
+# forward(50)
+# left(90)
+# forward(50)
+# left(90)
+# down()
+# # draw the square
+# forward(100)
+# left(90)
+# forward(100)
+# left(90)
+# forward(100)
+# left(90)
+# forward(100)
+# mainloop()
+
+# Exercise 1 - Simple Shapes
+# Equilateral Triangle
+# from turtle import *
+# setheading(270)
+# forward(150)
+# right(120)
+# forward(150)
+# right(120)
+# forward(150)
+# mainloop()
+
+#Square
+# from turtle import *
+# goto (0,0)
+
+# for x in range(4):
+#     forward(200)
+#     right(90)
+# mainloop()
+
+#pentagon
+# from turtle import *
+# goto (0,0)
+# setheading(180)
+
+# for x in range(5):
+#     forward(200)
+#     right(72)
+# mainloop()
+
+
+#hexagon
+# from turtle import *
+# goto (0,0)
+# setheading(180)
+
+# for x in range(6):
+#     forward(150)
+#     right(60)
+# mainloop()
+
+
+#octagon
+# from turtle import *
+# goto (0,0)
+# setheading(180)
+
+# for x in range(8):
+#     forward(100)
+#     right(45)
+# mainloop()
+
+
+#star
+# from turtle import *
+# forward(200)
+# right(144)
+# forward(200)
+# right(144)
+# forward(200)
+# right(144)
+# forward(200)
+# right(144)
+# forward(200)
+# right(144)
+# mainloop()
+
+#circle
+# from turtle import *
+# circle (100)
+# mainloop()
+
+#Exercise 2 - will talk about modules in class
+#Exercise 3 - will talk about modules in class
+
+#Exercise 4 - need to revisit
+# from turtle import *
+# def makestamp():
+#     setheading(random.randint(1,200))
+#     pensize(random.randint(1, 7))
+#     color(random.choice(myColourList))
+# bgcolor("LightSteelBlue")
+# for i in range(10):
+#     x,y = random.randint(-50, 50), random.randint(-50, 50)
+#     shape(random.choice(myShapeList))
+#     mainloop()
+# mainloop()
+
+
+#Artistic Project 1
+# from turtle import *
+# goto (0,0)
+# setheading(90)
+
+
+# for x in range(5):
+#     forward(125)
+#     right(90)
+
+# left(45)
+# forward(90)
+# right(90)
+# forward(90)
+
+# penup()
+# goto (20,70)
+# setheading(90)
+# pendown()
+# for i in range(4):
+#     forward (20)
+#     right(90)
+
+# penup()
+# goto (85,70)
+# setheading(90)
+# pendown()
+# for i in range(4):
+#     forward (20)
+#     right(90)
+
+# penup()
+# goto (50,0)
+# setheading(90)
+# pendown()
+# forward(30)
+# right(90)
+
+# penup()
+# goto (75,0)
+# setheading(90)
+# pendown()
+# forward(30)
+# left(90)
+# forward(25)
+
+# penup()
+# goto (62.5,0)
+# setheading(90)
+# pendown()
+# forward(30)
+
+# penup()
+# goto(-10,-10)
+
+# mainloop()
